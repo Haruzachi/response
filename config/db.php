3f7c1825f21b864167d16f43192a5a01d985a1b3
haruzachi <haruzachi@gmail.com>
2025-09-19 00:41:41 +0800

    update all changes

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $dbname = "emergency_system";
 $username = "root";
-$password = "";
+$password = "Admin123";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
