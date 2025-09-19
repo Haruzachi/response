c716ce2e93c3d34b1e5c07123279700df23ecdfe
haruzachi <haruzachi@gmail.com>
2025-09-19 18:02:43 +0800

    update all changes

--- staff/Sub1/logging.php
+++ staff/Sub1/logging.php
@@ -321,7 +321,7 @@ $locations = $query->fetchAll(PDO::FETCH_ASSOC);
 
   <style>
     #map {
-    height: calc(110vh - 110px); /* same as your container div */
+    height: calc(108vh - 108px); /* same as your container div */
     border-radius: 0.5rem;
     z-index: 0;
 }
