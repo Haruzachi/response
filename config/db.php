10cd6681f7323de444f33bc060157a1237227621
haruzachi <haruzachi@gmail.com>
2025-09-23 10:53:43 +0800

    update all changes

--- config/db.php
+++ config/db.php
@@ -1,5 +1,22 @@
 <?php
 $host = "localhost";
+$dbname = "emergency_system";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
+
+
+<!---
+
+
+$host = "localhost";
 $dbname = "er_es";
 $username = "er_es";
 $password = "Admin123";
