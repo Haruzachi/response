1f064421fc60617e6326168d450a0012feb2aa35
haruzachi <haruzachi@gmail.com>
2025-10-08 19:28:51 +0800

    update all changes

--- staff/Sub8/Map.php
+++ staff/Sub8/Map.php
@@ -59,7 +59,7 @@ $feedbacks = $query->fetchAll(PDO::FETCH_ASSOC);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Feedback Forms for Responders</title>
+  <title>Hazard Mapping</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="icon" type="../../image/x-icon" href="../../img/Logocircle.png">
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
