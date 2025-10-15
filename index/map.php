1d54103d39c1ccd62893acedf526a287ed1cf470
haruzachi <haruzachi@gmail.com>
2025-10-16 00:19:40 +0800

    update

--- index/map.php
+++ index/map.php
@@ -59,7 +59,7 @@
       border: none;
       border-radius: 8px;
       width: calc(100% - 80px); /* Reduced width */
-      margin: 0 40px;
+      margin: 0 20px;
       padding: 12px;
       font-size: 15px;
       font-weight: 500;
